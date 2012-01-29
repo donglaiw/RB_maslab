b9db921fbf908ee59c1a03ce7deba3af570aaf54
eminemya <weiddoonngglai@gmail.com>
2012-01-29 18:28:38 -0500

    week3

--- test/m_port.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("/home/maslab-team-8/Desktop/Dropbox/Maslab/software/BotKiller/Motor")
-import Control
-import Sensor
-import time
-
-ard=Control.Arduino()
- 
-ard.start()
